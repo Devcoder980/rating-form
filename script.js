324f6e8c6eee87f0a37da404efe1e39c669c63ac
prabhudev bind <106137526+Devcoder980@users.noreply.github.com>
2022-06-06 16:41:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function submit() {
+    document.getElementById("box").style.display = 'none';
+    document.getElementById("box2").style.display = 'block';
+}
+function callchange() {
+    document.getElementById("call").style.backgroundColor = "#fc7613";
+    document.getElementById("rating-value").innerHTML=1;
+}
+function callchange1() {
+    document.getElementById("call1").style.backgroundColor = "#fc7613";
+    document.getElementById("rating-value").innerHTML=2;
+}
+function callchange2() {
+    document.getElementById("call2").style.backgroundColor = "#fc7613";
+    document.getElementById("rating-value").innerHTML=3;
+}
+function callchange3() {
+    document.getElementById("call3").style.backgroundColor = "#fc7613";
+    document.getElementById("rating-value").innerHTML=4;
+}
+function callchange4() {
+    document.getElementById("call4").style.backgroundColor = "#fc7613";
+    document.getElementById("rating-value").innerHTML=5;
+}
+
